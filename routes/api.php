5de82ec8fa46c9ac380deb0b9484fac958447a4f
Fikri Hidayat <fikrihidayat799@gmail.com>
2026-03-01 13:08:49 +0700

    change route type

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post("/logout", [AuthController::class,"logout"]);
     Route::get('/claims/get-list', [ClaimController::class, 'index']);
     Route::post('/claims/store', [ClaimController::class, 'store'])->middleware('role:user');
-    Route::post('/claims/{id}/verify', [ClaimController::class, 'verify'])->middleware('role:verifier');
-    Route::post('/claims/{id}/approve', [ClaimController::class, 'approve'])->middleware('role:approver');
-    Route::post('/claims/{id}/reject', [ClaimController::class, 'reject'])->middleware('role:approver');
+    Route::put('/claims/{id}/verify', [ClaimController::class, 'verify'])->middleware('role:verifier');
+    Route::put('/claims/{id}/approve', [ClaimController::class, 'approve'])->middleware('role:approver');
+    Route::put('/claims/{id}/reject', [ClaimController::class, 'reject'])->middleware('role:approver');
 });
